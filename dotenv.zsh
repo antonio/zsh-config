938c74cbb7de24f779dac3980f59e83a53aad3bb
Antonio Santos <antonio@santosvelasco.com>
2014-11-01 00:39:58 +0100

    On enter and exit cwd hooks

--- dotenv.zsh
+++ dotenv.zsh
@@ -1,5 +1,20 @@
-function source_dotenv {
-  [[ -f $PWD/.env ]] && source $PWD/.env
+#!/bin/zsh
+
+on_enter_hook() {}
+on_exit_hook() {}
+
+dotenv () {
+  on_exit_hook
+  if [[ -a $PWD/.env ]]; then
+    grep on_enter_hook $PWD/.env >/dev/null 2>&1 || eval "on_enter_hook() {}"
+    grep on_exit_hook $PWD/.env >/dev/null 2>&1  || eval "on_exit_hook() {}"
+
+    . $PWD/.env
+  else
+    eval "on_enter_hook() {}"
+    eval "on_exit_hook() {}"
+  fi
+  on_enter_hook
 }
 
-chpwd_functions+=source_dotenv
+chpwd_functions+=dotenv
